70439dfdf09cff9a7e4757983eaff201e05fae85
Li <736872721@qq.com>
2019-05-29 15:06:46 +0800

    文文瑾子的可爱创作（づ￣3￣）づ╭❤～

--- app/src/main/java/com/xl/kuxingtx/fragment/Mine/FragmentMine.java
+++ app/src/main/java/com/xl/kuxingtx/fragment/Mine/FragmentMine.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -27,11 +29,31 @@ public class FragmentMine extends Fragment implements  View.OnClickListener, FMi
     @ViewInject(R.id.editText)
     private EditText ed2;
 
+    //登录的按钮
+    @ViewInject(R.id.button6)
+    private Button btn1;
+
+    //忘记密码的按钮
+    @ViewInject(R.id.button4)
+    private Button btn2;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=LayoutInflater.from(getActivity()).inflate(R.layout.fragment_fragment_mine,null);
         ViewUtils.inject(getActivity());
+
+        //登录的监听事件
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String account=ed1.getText().toString();
+                String pwd=ed2.getText().toString();
+                Log.e("","我他妈要上天！！！我他妈要上天！！！我他妈要上天！！！我他妈要上天！！！我他妈要上天！！！+account");
+                Log.e("","我他妈不要上天！！！我他妈要上天！！！我他妈要上天！！！我他妈要上天！！！我他妈要上天！！！+account");
+                //对model层的接口进行调用
+            }
+        });
         return view;
     }
 
@@ -57,4 +79,6 @@ public class FragmentMine extends Fragment implements  View.OnClickListener, FMi
                 break;
         }*/
     }
+
+
 }
