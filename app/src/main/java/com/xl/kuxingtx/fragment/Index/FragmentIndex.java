790f56c82fa372a5d7f596deebc1b21847049915
Mr-ztt <yttandzt@gmail.com>
2019-06-03 10:09:32 +0800

    地图未开发完

--- app/src/main/java/com/xl/kuxingtx/fragment/Index/FragmentIndex.java
+++ app/src/main/java/com/xl/kuxingtx/fragment/Index/FragmentIndex.java
@@ -4,14 +4,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.LocationSource;
 import com.amap.api.maps2d.MapView;
-import com.lidroid.xutils.ViewUtils;
+import com.amap.api.maps2d.UiSettings;
 import com.xl.kuxingtx.R;
-
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
@@ -23,24 +29,71 @@ public class FragmentIndex extends Fragment implements View.OnClickListener {
     public TextView home_city;
     @ViewInject(R.id.map)
     public MapView mMapView;
+
+    /**
+     * 以下是涛涛的变量
+     */
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    public AMap aMap = null;
+
+    private LocationSource.OnLocationChangedListener mListener;
+
+    //分界线
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = x.view().inject(this, inflater, container);
         home_city.setOnClickListener(this);
-
-        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        if (aMap == null) {
+            aMap = mMapView.getMap();
+        }
         mMapView.onCreate(savedInstanceState);
+        setUpLocation();
+        //aMap.moveCamera(CameraUpdateFactory.zoomTo(16));
         return view;
     }
 
+    /**
+     * 初始化定位条件
+     */
+    private void setUpLocation() {
+        if (aMap == null) {
+            aMap = mMapView.getMap();
+            //设置ui条件
+        }
+        UiSettings mUiSettings = aMap.getUiSettings();
+        mUiSettings.setMyLocationButtonEnabled(true);
+        aMap.setLocationSource(mLocationSource);
+        aMap.setMyLocationEnabled(true);
+    }
+
+
+    /**
+     * 实现定位
+     */
+    public void setLocation() {
+        mLocationClient = new AMapLocationClient(this.getContext());
+        //设置监听回调
+        mLocationClient.setLocationListener(mAMapLocationListener);
+        AMapLocationClientOption mLocationOption = new AMapLocationClientOption();
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        mLocationOption.setInterval(2000);
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        if (mLocationClient != null) {
+            mLocationClient.stopLocation();
+            mLocationClient.startLocation();
+        }
+    }
 
     @Override
     public void setMenuVisibility(boolean menuVisible) {
         super.setMenuVisibility(menuVisible);
-        if(this.getView()!=null){
-            this.getView().setVisibility(menuVisible?View.VISIBLE:View.GONE);
+        if (this.getView() != null) {
+            this.getView().setVisibility(menuVisible ? View.VISIBLE : View.GONE);
         }
     }
 
@@ -57,4 +110,43 @@ public class FragmentIndex extends Fragment implements View.OnClickListener {
                 break;
         }*/
     }
+
+    /**
+     * 部分变量
+     */
+    public LocationSource mLocationSource = new LocationSource() {
+        @Override
+        public void activate(OnLocationChangedListener onLocationChangedListener) {
+            mListener = onLocationChangedListener;
+            setLocation();
+        }
+
+        @Override
+        public void deactivate() {
+            mListener = null;
+            if (mLocationClient != null) {
+                mLocationClient.stopLocation();
+                mLocationClient.onDestroy();
+            }
+            mLocationClient = null;
+        }
+    };
+
+    public AMapLocationListener mAMapLocationListener = new AMapLocationListener() {
+        /**
+         * 设置监听位置改变
+         *
+         * @param aMapLocation
+         */
+        @Override
+        public void onLocationChanged(AMapLocation aMapLocation) {
+            if (aMapLocation != null) {
+                if (aMapLocation.getErrorCode() == 0) {
+                    //可在其中解析amapLocation获取相应内容。
+                    mListener.onLocationChanged(aMapLocation);
+                } else Log.e("AmapError", "location Error, ErrCode:" + aMapLocation.getErrorCode() +
+                        ", errInfo:" + aMapLocation.getErrorInfo());
+            }
+        }
+    };
 }
